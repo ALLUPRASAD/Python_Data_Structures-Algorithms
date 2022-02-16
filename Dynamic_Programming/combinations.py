e24972948cd1f2a7c97e9e3b32e92389a22364c8
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-02-16 16:08:41 +0530

    Update combinations.py

--- Dynamic_Programming/combinations.py
+++ Dynamic_Programming/combinations.py
@@ -1,1 +1,14 @@
-
+def combinations(str,max_len):
+  if len(str)==0:
+    print(max_len)
+    return
+  for i in range(len(str)):
+    ch=str[i]
+    left=str[0:i]
+    right=str[i+1:]
+    result=left+right
+    combinations(result,max_len+ch)
+    
+str='prasad'
+max_len=''
+combinations(str,max_len)
