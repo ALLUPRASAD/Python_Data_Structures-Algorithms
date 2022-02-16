55a5bfdbe125dbdc8f6a794a7c0393f49fa6a692
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-02-16 16:23:01 +0530

    Update length_subarray_start&end_same_letter.py

--- Arrays/length_subarray_start&end_same_letter.py
+++ Arrays/length_subarray_start&end_same_letter.py
@@ -1,1 +1,12 @@
 
+#brute_force 
+
+str='xrrdfsdvdfvfb'
+max_s=0
+for i in range(len(str)):
+    for j in range(len(str)+1):
+        sub_string=str[i:j]
+        if sub_string!='':
+            if (sub_string[0]==sub_string[-1]):
+                max_s=max(max_s,len(str[i:j]))
+print(max_s)
