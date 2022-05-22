5a3342dee80ec7b30349911d51cb0c4239b59f30
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-05-22 11:30:56 +0530

    Update combinations.py

--- Permuntations&Combinations/combinations.py
+++ Permuntations&Combinations/combinations.py
@@ -12,3 +12,25 @@ string = "asx"
 n = len(string)
 a = list(string)
 permut(a, 0, n-1)  
+
+
+# write a code to Print all combinations of x 
+# output example: rkishna,shnairk
+
+x="abc"
+
+def permute(x):
+  result = []
+  if len(x) == 0:
+    return []
+  
+  if len(x) == 1:
+    return [x]
+  
+  for i in range(len(x)):
+    for j in permute(x[:i]+x[i+1:]):
+        result.append(x[i] + j)
+  
+  return result
+
+permute(x)
