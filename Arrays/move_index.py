c3859442bef6ecae6918356f6bc12f0f188ac94d
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-05-30 16:26:17 +0530

    Update move_index.py

--- Arrays/move_index.py
+++ Arrays/move_index.py
@@ -1,1 +1,14 @@
 
+def move_ones(nums):
+    index=0
+    for num in nums:
+        if num!=1:
+            nums[index]=num
+            index=index+1
+    for nu in range(index,len(nums)):
+        nums[nu]=1
+    return nums
+  
+#   input:nums=[1,1,0,8,7,6,5]
+#   move_ones(nums)
+#   output:[0, 8, 7, 6, 5, 1, 1]
