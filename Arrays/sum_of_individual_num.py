9654c71d8658c26bfe23f0ed64936df38e0f11a3
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-05-30 16:16:34 +0530

    Update sum_of_individual_num.py

--- Arrays/sum_of_individual_num.py
+++ Arrays/sum_of_individual_num.py
@@ -1,1 +1,20 @@
-
+#1st method
+def input_num(num):
+    s=0
+    x=num
+    while x>0:
+        y=x%10
+        x=x//10
+        s=s+y
+    return s
+  
+  # recursive approach
+  
+def addDigits(num):
+    nums = list(map(int, str(num)))
+    if len(nums)>1:
+        nums = sum(nums)
+        nums = addDigits(nums)
+    else:
+        nums = nums[0]
+    return nums
