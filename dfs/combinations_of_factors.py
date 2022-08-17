9dc1748c683438441d58626a87baf146eabc1852
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-08-17 20:56:50 +0530

    Update combinations_of_factors.py

--- dfs/combinations_of_factors.py
+++ dfs/combinations_of_factors.py
@@ -1,1 +1,13 @@
 
+from math import sqrt
+def get_factors(n):
+    results=[]
+    def dfs(n,factor,path):
+        for num in range(2,int(sqrt(n))+1):
+            if n%num==0:
+                divisor=n//num
+                results.append(path+[divisor,num])
+                dfs(divisor,num,path+[num])
+    dfs(n,2,[])
+    return results
+get_factors(20)
