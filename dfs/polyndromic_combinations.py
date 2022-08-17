b18e3e711e23e26af777474b4bda4392248e8707
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-08-17 22:33:45 +0530

    Create polyndromic_combinations.py

---
+++ dfs/polyndromic_combinations.py
@@ -0,0 +1,21 @@
+#brute force approach 
+class Solution(object):
+    def generatePalindromes(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        self.results=[]
+        p_results=[]
+        def dfs(st,resul):
+            if len(st)==0:
+                if resul==resul[::-1]:
+                    self.results.append(resul)
+            for i in range(len(st)):
+                ch=st[i]
+                l=st[0:i]
+                r=st[i+1:]
+                res=l+r
+                dfs(res,ch+resul)
+        dfs(s,'')
+        return set(self.results)
