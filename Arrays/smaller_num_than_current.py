4ee0b98df4d6405347a798aa45b86d6b101cb3a1
ALLUPRASAD <56666744+ALLUPRASAD@users.noreply.github.com>
2022-09-22 21:23:14 +0530

    Update smaller_num_than_current.py

--- Arrays/smaller_num_than_current.py
+++ Arrays/smaller_num_than_current.py
@@ -8,3 +8,17 @@ class Solution:
                     c=c+1
             lst.append(c)
         return lst
+#o(2n)
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        d={}
+        res=[]
+        sorted_nums=sorted(nums)
+        for i in range(len(nums)):
+            if sorted_nums[i] not in d:
+                d[sorted_nums[i]]=i
+
+        for k in nums:
+            res.append(d[k])
+        return res  
