c5db702ff26c52087c69ca04c11d9874cb35158e
allu prasad <alluprasad@allus-MacBook-Air.local>
2024-01-04 19:45:15 +0530

    xor property

---
+++ DSA_Patterns/interview_problems/unique_num.py
@@ -0,0 +1,14 @@
+# xor has assosiative property not required sorting 1^1=0,1^2=2^1,0^1=0
+def unique(x):
+    ans=0
+    for i in range(len(x)):
+        ans=ans^x[i]
+    return ans
+
+
+if __name__ == "__main__":
+    x=[1,2,1,2,7,8,9,7,9,9,9]
+    d=unique(x)
+    print(d)
+
+
